5c5a705c73facf9615279224f280b2470e5d3344
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-08-25 07:40:22 -0400

    ReadME Changes and Refactoring frontend.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# GomuCloud
+# Gomu Cloud
+
+
+## Ideas
+Some of the feature ideas that should be implemented in the Gomu cloud.
+
+
+### App Wide
+1. User Management System
+
+### DDaily Journal App
+1. Routine checker
+    - Routine setup. Individual Routine Check.
+        - Weekdays, Weekdays, Everyday.
+        - Until when?
+        - Reminder?
+1. TODO List
+1. Day Summary Display
+    - Displays
+        - Day of the week, day of the month, month, and year.
+        - Routine
+        - TODO
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
