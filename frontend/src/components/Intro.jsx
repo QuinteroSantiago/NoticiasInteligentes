1afba1da3f469aee6c3313dc6452b748a6d7c94f
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-12 18:00:22 -0500

    Added Timekeeper feature in Spanish

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Intro() {
     return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-3">
             <h1 className="text-5xl md:text-7xl dark:text-black mb-1 md:mb-3 font-bold">Noticias Inteligentes</h1>
             <p className="text-md max-w-xl mb-6">
                 Las noticias buenas del día! Traídas por la Inteligencia Artificial.
