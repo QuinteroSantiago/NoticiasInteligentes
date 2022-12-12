8c3acbed43f05b9ac4b54641e59fbc0ff22ab971
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-12 14:04:04 -0500

    Add 6 news for MVP

--- frontend/src/data/newsData.js
+++ frontend/src/data/newsData.js
@@ -11,4 +11,28 @@ export default [
       stack: ['Francia', 'Agua Potable'],
       link: 'https://lacarabuenadelmundo.com/inventan-un-sistema-para-potabilizar-agua-en-cualquier-parte/',
    },
+   {
+      title: 'Científicos de Harvard descubren el secreto de la felicidad',
+      imgUrl: '/assets/noticia3.avif',
+      stack: ['Felicidad', 'Harvard'],
+      link: 'https://lacarabuenadelmundo.com/cientificos-de-harvard-descubren-el-secreto-de-la-felicidad/',
+   },
+   {
+      title: 'Descubierto uno de los mayores arrecifes de coral del mundo',
+      imgUrl: '/assets/noticia4.png',
+      stack: ['Arrecifes','Océanos', 'Tahití'],
+      link: 'https://www.sonbuenasnoticias.com/destacado/descubierto-uno-de-los-mayores-arrecifes-de-coral-del-mundo/',
+   },
+   {
+      title: 'Importante avance científico para la prevención de la esclerosis múltiple',
+      imgUrl: '/assets/noticia5.jpeg',
+      stack: ['Ciencia', 'Harvard'],
+      link: 'https://www.sonbuenasnoticias.com/destacado/importante-avance-cientifico-para-la-prevencion-de-la-esclerosis-multiple/',
+   },
+   {
+      title: 'Marruecos: Primer país africano que llega a una semifinal del Mundial!',
+      imgUrl: '/assets/noticia6.jpeg',
+      stack: ['Marruecos','Mundial 2022'],
+      link: 'https://www.bbc.com/mundo/noticias-63930291',
+   },
 ] 
