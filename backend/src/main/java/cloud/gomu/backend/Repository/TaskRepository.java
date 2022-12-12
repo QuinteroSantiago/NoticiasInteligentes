51c0f667e67cf34d1c4eccf1a6a8c85d96b70288
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-12 12:58:25 -0500

    Removed different backend

--- backend/src/main/java/cloud/gomu/backend/Repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package cloud.gomu.backend.Repository;
-
-import cloud.gomu.backend.Entity.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
