e9c60e4a7f4b5146c4191646fb32fe93b4d53442
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-12 16:11:05 -0500

    Changed tags syntax, changed scam image

--- frontend/src/data/newsData.js
+++ frontend/src/data/newsData.js
@@ -1,38 +1,38 @@
 export default [
     {
       title: 'Atrae emociones positivas y ten una vida mas plena',
-      imgUrl: '/assets/noticia1.png',
-      stack: ['Bienestar',],
+      imgUrl: '/assets/noticia1.jpeg',
+      tags: ['Bienestar',],
       link: 'https://lacarabuenadelmundo.com/como-atraer-emociones-positivas-tener-una-vida-mas-plena/',
    },
    {
       title: 'Un francés inventa acceso al agua potable para todas partes del mundo',
       imgUrl: '/assets/noticia-2.jpeg',
-      stack: ['Francia', 'Agua Potable'],
+      tags: ['Francia', 'Agua Potable'],
       link: 'https://lacarabuenadelmundo.com/inventan-un-sistema-para-potabilizar-agua-en-cualquier-parte/',
    },
    {
       title: 'Científicos de Harvard descubren el secreto de la felicidad',
       imgUrl: '/assets/noticia3.avif',
-      stack: ['Felicidad', 'Harvard'],
+      tags: ['Felicidad', 'Harvard'],
       link: 'https://lacarabuenadelmundo.com/cientificos-de-harvard-descubren-el-secreto-de-la-felicidad/',
    },
    {
       title: 'Descubierto uno de los mayores arrecifes de coral del mundo',
       imgUrl: '/assets/noticia4.png',
-      stack: ['Arrecifes','Océanos', 'Tahití'],
+      tags: ['Arrecifes','Océanos', 'Tahití'],
       link: 'https://www.sonbuenasnoticias.com/destacado/descubierto-uno-de-los-mayores-arrecifes-de-coral-del-mundo/',
    },
    {
       title: 'Importante avance científico para la prevención de la esclerosis múltiple',
       imgUrl: '/assets/noticia5.jpeg',
-      stack: ['Ciencia', 'Harvard'],
+      tags: ['Ciencia', 'Harvard'],
       link: 'https://www.sonbuenasnoticias.com/destacado/importante-avance-cientifico-para-la-prevencion-de-la-esclerosis-multiple/',
    },
    {
       title: 'Marruecos: Primer país africano que llega a una semifinal del Mundial!',
       imgUrl: '/assets/noticia6.jpeg',
-      stack: ['Marruecos','Mundial 2022'],
+      tags: ['Marruecos','Mundial 2022'],
       link: 'https://www.bbc.com/mundo/noticias-63930291',
    },
 ] 
