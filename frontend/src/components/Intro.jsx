9ec94b5fd71ed38934f72b133f85b313a6886892
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-12 13:36:03 -0500

    MVP with 2 links

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-5xl md:text-7xl dark:text-black mb-1 md:mb-3 font-bold">Noticias Inteligentes</h1>
             <p className="text-md max-w-xl mb-6">
-            Las noticias buenas del día, traídas por la Inteligencia Artificial
+                Las noticias buenas del día! Traídas por la Inteligencia Artificial.
                 <br />
             </p>
         </div>
