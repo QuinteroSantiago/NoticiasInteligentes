84517c844989253ef1a8cfcc4bcdf0de0659a694
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-12 13:06:13 -0500

    Changing branding to Noticias Inteligentes

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-5xl md:text-7xl dark:text-black mb-1 md:mb-3 font-bold">Gomu Cloud</h1>
-            <p className="text-base md:text-2xl mb-3 font-medium">Santiago Quintero</p>
+            <h1 className="text-5xl md:text-7xl dark:text-black mb-1 md:mb-3 font-bold">Noticias Inteligentes</h1>
             <p className="text-md max-w-xl mb-6">
-                I plan to use this as a portfolio for all the things I want to do.
+            Las noticias buenas del día, traídas por la Inteligencia Artificial
                 <br />
             </p>
         </div>
