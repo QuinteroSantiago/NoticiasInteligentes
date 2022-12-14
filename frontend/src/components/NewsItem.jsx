e8218c84db92d9b1ca27c1f4cf748472cd289276
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-14 09:42:51 -0500

    Reformatted news articles's date on UI

--- frontend/src/components/NewsItem.jsx
+++ frontend/src/components/NewsItem.jsx
@@ -8,19 +8,21 @@ function NewsItem({ title, imgUrl, tags, link, date }) {
          rel="noopener noreferrer"
          className="border-2 border-stone-900 dark:border-black rounded-md overflow-hidden"
       >
-         <p className="flex justify-end text-xs md:text-sm">
-            <span>
-               {date}
-            </span>
-         </p>
-         <img
-            src={imgUrl}
-            alt={title}
-            className="w-full h-36 md:h-48 object-cover cursor-pointer"
-         />
+         <div class="relative">
+            <img
+               src={imgUrl}
+               alt={title}
+               className="w-full h-36 md:h-48 object-cover cursor-pointer"
+            />
+            <p className="absolute text-xs top-3 right-3 dark:text-white">
+               <span className="inline-block px-1 border-2 dark:border-black bg-stone-800 opacity-70 rounded-lg">
+                  {date}
+               </span>
+            </p>
+         </div>
          <div className="w-full p-2">
             <h3 className="text-lg md:text-xl dark:text-black mb-2 md:mb-3 font-semibold">{title}</h3>
-            <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-black ">
+            <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-black">
                {tags.map(item => (
                   <span className="inline-block px-2 py-1 border-2 border-stone-900 dark:border-black rounded-md">
                      {item}
