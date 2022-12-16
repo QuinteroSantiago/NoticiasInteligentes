65e8a3031a7bb3213bbddc6afd87a3a1a7ada769
Santiago Quintero <santiagoquinteroq@gmail.com>
2022-12-16 09:42:04 -0500

    Higher contrast text for easier to read text in Footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
 		<div className="py-5 text-center">
-			<p className="text-sm mt-2  opacity-50">
+			<p className="text-sm mt-3 opacity-80">
 				&copy; {new Date().getFullYear()} Santiago Quintero. All rights reserved.
 			</p>
 		</div>
