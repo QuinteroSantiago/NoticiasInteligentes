6af578773dc179e0ab68cfd83b118e54d3d71db0
Santiago Quintero <santiago@corellium.com>
2024-07-18 08:44:43 -0400

    change path

--- backend/src/routers/all_articles.py
+++ backend/src/routers/all_articles.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 @router.get("/otros")
 async def ultimo():
-    file_path = os.path.abspath('/Users/santiagoquintero/Desktop/projects/NoticiasInteligentes/backend/src/scripts/news_articles.json')  
+    file_path = os.path.abspath('/vault/bit-marina/marina-labs/repos/NoticiasInteligentes/backend/src/scripts/news_articles.json')  
     with open(file_path) as f:
         data = json.load(f)
     return data
