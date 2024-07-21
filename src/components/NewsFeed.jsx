7557f54557b5e18dba97141e1efb8f320b1ec1bc
Santiago Quintero <santiago@gomu.cloud>
2024-07-21 11:15:51 -0400

    Modify components

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import newsData from '../data/pythonNewsData';
+import React, { useState } from 'react';
+import newsData from '../data/news_articles';
 import NewsItem from './NewsItem';
 
 function NewsFeed() {
+   const [filter, setFilter] = useState('all'); // State to control the filter
+
+   const handleFilterChange = (newFilter) => {
+      setFilter(newFilter);
+   };
+
+   const filteredNewsData = newsData.filter(newsItem => {
+      if (filter === 'positive') return newsItem.sentiment_score >= -0.1;
+      if (filter === 'negative') return newsItem.sentiment_score < 0;
+      return true; // 'all' or default case
+   });
+
    return (
       <div className="flex flex-col items-center justify-center px-10">
+         <div className="mb-4">
+            <button onClick={() => handleFilterChange('all')} className="mx-2 px-4 py-2 bg-gray-200 rounded">Todas las noticias</button>
+            <button onClick={() => handleFilterChange('positive')} className="mx-2 px-4 py-2 bg-green-200 rounded">Noticias positivas</button>
+            <button onClick={() => handleFilterChange('negative')} className="mx-2 px-4 py-2 bg-red-200 rounded">Noticias negativas</button>
+         </div>
          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-            {newsData.map((project) => (
+            {filteredNewsData.map((newsItem) => (
                <NewsItem
-                  key={project.link}
-                  imgUrl={project.image_url}
-                  title={project.title}
-                  tags={project.keywords}
-                  link={project.link}
-                  date={project.pubDate}
-                  sentimentScore={project.sentiment_score}
+                  key={newsItem.link}
+                  imgUrl={newsItem.image_url}
+                  title={newsItem.title}
+                  tags={newsItem.tags}
+                  link={newsItem.link}
+                  date={newsItem.date}
+                  sentimentScore={newsItem.sentiment_score}
                />
             ))}
          </div>
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,15 +1,29 @@
 import { useState, useEffect } from 'react';
 
 function NewsItem({ title, imgUrl, tags, link, date, sentimentScore }) {
-   const [borderColor, setBorderColor] = useState('border-red-500');
    const [imageUrl, setImageUrl] = useState('/assets/placeholder-for-na.png');
+   const [positivityScore, setPositivityScore] = useState(50); // Default at neutral (50)
+   const [textColor, setTextColor] = useState('text-black'); // Default text color
 
    useEffect(() => {
-      if (sentimentScore >= 0.2) {
-         setBorderColor('border-green-500');
-      } else if (sentimentScore >= 0) {
-         setBorderColor('border-stone-900');
+
+      // Calculate positivity score from sentimentScore and update text color
+      const calculatedScore = ((sentimentScore + 1) / 2) * 100;
+      setPositivityScore(calculatedScore);
+
+      console.log('calculatedScore: ', calculatedScore)
+      if (calculatedScore >= 75) {
+         setTextColor('text-green-500');
+      } else if (calculatedScore > 51) {
+         setTextColor('text-green-200');
+      } else if (calculatedScore > 49) {
+         setTextColor('text-white');
+      } else if (calculatedScore >= 25) {
+         setTextColor('text-red-200');
+      } else {
+         setTextColor('text-red-500');
       }
+
    }, [sentimentScore]);
 
    useEffect(() => {
@@ -23,7 +37,7 @@ function NewsItem({ title, imgUrl, tags, link, date, sentimentScore }) {
          href={link}
          target="_blank"
          rel="noopener noreferrer"
-         className={`border-4 ${borderColor} rounded-md overflow-hidden w-full md:w-60`}
+         className={`border-2 border-stone-500 rounded-md overflow-hidden w-full md:w-60 shadow-lg transition-shadow hover:shadow-xl`}
       >
          <div className="relative">
             <img
@@ -31,16 +45,17 @@ function NewsItem({ title, imgUrl, tags, link, date, sentimentScore }) {
                alt={title}
                className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
-            <p className="absolute top-2 right-2 text-white text-sm">
-               <span className="px-2 py-1 border-2 bg-stone-800 opacity-75 rounded-lg">
-                  {date}
-               </span>
+            <p className="absolute top-2 right-2 text-white text-sm bg-stone-800 opacity-75 rounded-lg px-2 py-1">
+               {date}
+            </p>
+            <p className={`absolute bottom-2 left-2 text-lg font-bold bg-stone-700 opacity-90 rounded-lg px-2 py-1 ${textColor}`}>
+               {`${positivityScore.toFixed(0)}% Positivo`}
             </p>
          </div>
          <div className="p-2">
             <h3 className="text-lg md:text-xl mb-2 font-semibold">{title}</h3>
             <div className="flex flex-wrap gap-2">
-               {tags.map((item, index) => (
+               {tags.slice(0, 5).map((item, index) => (
                   <span
                      key={index}
                      className="px-2 py-1 border-2 border-stone-900 rounded-md text-xs md:text-sm"
