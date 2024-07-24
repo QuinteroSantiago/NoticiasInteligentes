f76eb6c8a0b7cdb37cf4767662b0bb5ee28d1d20
Santiago Quintero <santiago@gomu.cloud>
2024-07-24 14:46:39 -0400

    chore: Change default sort to by date

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -13,7 +13,7 @@ function normalizeText(text) {
 
 function NewsFeed() {
    const [filter, setFilter] = useState(localStorage.getItem('newsFilter') || 'positive');
-   const [sortMethod, setSortMethod] = useState(localStorage.getItem('newsSortMethod') || 'most_positive');
+   const [sortMethod, setSortMethod] = useState(localStorage.getItem('newsSortMethod') || 'date');
    const [currentPage, setCurrentPage] = useState(1);
    const [itemsPerPage, setItemsPerPage] = useState(50);
    const [paginatedData, setPaginatedData] = useState([]);
