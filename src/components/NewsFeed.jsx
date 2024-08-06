666c367ca4ae0f1dd1c7d65fda52daa24a67c433
Santiago Quintero <santiago@gomu.cloud>
2024-08-06 19:30:25 -0400

    chore: new way of loading news articles

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -32,12 +32,11 @@ function NewsFeed() {
    const [numPages, setNumPages] = useState(0);
 
    useEffect(() => {
-      // Fetch the news data from the dist/data directory
-      fetch('/data/news_articles.json')
+      fetch('assets/data/news_articles.json')
          .then(response => response.json())
          .then(data => setNewsData(data))
          .catch(error => console.error('Error loading news data:', error));
-    }, []);
+   }, []);
 
    useEffect(() => {
         let filteredData = newsData.filter(newsItem => {
