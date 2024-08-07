aab3a4bfde7f3b23f423246ad554f906d7f1e642
Santiago Quintero <santiago@gomu.cloud>
2024-08-07 03:32:43 -0400

    chore: Update search bar placeholder

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,8 +4,8 @@ function SearchBar({ searchTerm, onSearchChange }) {
     return (
         <input
             type="text"
-            placeholder="Search news..."
-            className="px-4 py-2 rounded border shadow mb-5" // Tailwind class for margin bottom
+            placeholder="Buscar noticias..."
+            className="px-4 py-2 rounded border shadow mb-5"
             value={searchTerm}
             onChange={e => onSearchChange(e.target.value)}
         />
